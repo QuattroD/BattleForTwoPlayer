71f89b4d094e6d2511b3d408f974bc68bf701365
vnsxd666@outlook.com <vnsxd666@outlook.com>
2022-12-02 22:11:25 +0300

    fix

--- BattleForTwoPlayer/MainWindow.xaml.cs
+++ BattleForTwoPlayer/MainWindow.xaml.cs
@@ -162,5 +162,143 @@ namespace BattleForTwoPlayer
                 mAttackValue.Text = $"{int.Parse(mAttackValue.Text) + 1}";
             }
         }
+
+        private void StrengthMinusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (StrengthSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) + 1}";
+                StrengthSecondPlayer.Text = $"{int.Parse(StrengthSecondPlayer.Text) - 1}";
+                pAttackValueSecondPlayer.Text = $"{int.Parse(pAttackValueSecondPlayer.Text) - 1}";
+            }
+        }
+
+        private void DexterityMinusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (DexteritySecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) + 1}";
+                DexteritySecondPlayer.Text = $"{int.Parse(DexteritySecondPlayer.Text) - 1}";
+            }
+        }
+
+        private void LuckMinusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (LuckSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) + 1}";
+                LuckSecondPlayer.Text = $"{int.Parse(LuckSecondPlayer.Text) - 1}";
+            }
+        }
+
+        private void ConstitutionMinusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (ConstitutionSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) + 1}";
+                ConstitutionSecondPlayer.Text = $"{int.Parse(ConstitutionSecondPlayer.Text) - 1}";
+                hpValueSecondPlayer.Text = $"{int.Parse(hpValueSecondPlayer.Text) - 5}";
+            }
+        }
+
+        private void IntelligenceMinusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (IntteligenceSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) + 1}";
+                IntteligenceSecondPlayer.Text = $"{int.Parse(IntteligenceSecondPlayer.Text) - 1}";
+                manaValueSecondPlayer.Text = $"{int.Parse(manaValueSecondPlayer.Text) - 7}";
+                mAttackValueSecondPlayer.Text = $"{int.Parse(mAttackValueSecondPlayer.Text) - 1}";
+            }
+        }
+
+        private void StrengthPlusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (FreePointsSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                StrengthSecondPlayer.Text = $"{int.Parse(StrengthSecondPlayer.Text) + 1}";
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) - 1}";
+                pAttackValueSecondPlayer.Text = $"{int.Parse(pAttackValueSecondPlayer.Text) + 1}";
+            }
+        }
+
+        private void DexterityPlusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (FreePointsSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                DexteritySecondPlayer.Text = $"{int.Parse(DexteritySecondPlayer.Text) + 1}";
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) - 1}";
+            }
+        }
+
+        private void LuckPlusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (FreePointsSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                LuckSecondPlayer.Text = $"{int.Parse(LuckSecondPlayer.Text) + 1}";
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) - 1}";
+            }
+        }
+
+        private void ConstitutionPlusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (FreePointsSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                ConstitutionSecondPlayer.Text = $"{int.Parse(ConstitutionSecondPlayer.Text) + 1}";
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) - 1}";
+                hpValueSecondPlayer.Text = $"{int.Parse(hpValueSecondPlayer.Text) + 5}";
+            }
+        }
+
+        private void IntelligencePlusSecondPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (FreePointsSecondPlayer.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                IntteligenceSecondPlayer.Text = $"{int.Parse(IntteligenceSecondPlayer.Text) + 1}";
+                FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) - 1}";
+                manaValueSecondPlayer.Text = $"{int.Parse(manaValueSecondPlayer.Text) + 7}";
+                mAttackValueSecondPlayer.Text = $"{int.Parse(mAttackValueSecondPlayer.Text) + 1}";
+            }
+        }
     }
 }
