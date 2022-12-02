a54be1788b2b62e6c0d11869e58005127d38ac26
vnsxd666@outlook.com <vnsxd666@outlook.com>
2022-12-03 00:56:49 +0300

    fix

--- BattleForTwoPlayer/MainWindow.xaml.cs
+++ BattleForTwoPlayer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,12 @@ namespace BattleForTwoPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<FighterModelFirstPlayer> fighters = new List<FighterModelFirstPlayer>();
+        public List<string> fightersName = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void StrengthMinus_Click(object sender, RoutedEventArgs e)
@@ -298,6 +302,150 @@ namespace BattleForTwoPlayer
                 FreePointsSecondPlayer.Text = $"{int.Parse(FreePointsSecondPlayer.Text) - 1}";
                 manaValueSecondPlayer.Text = $"{int.Parse(manaValueSecondPlayer.Text) + 7}";
                 mAttackValueSecondPlayer.Text = $"{int.Parse(mAttackValueSecondPlayer.Text) + 1}";
+            }
+        }
+
+        public bool ReadyFirst { get; set; }
+        public bool ReadySecond { get; set; }
+
+        private void Ready2_Click(object sender, RoutedEventArgs e)
+        {
+            if (Ready2.Content.ToString() == "Ready")
+            {
+                NameSecondPlayer.IsEnabled = false;
+                StrengthSecondPlayer.IsEnabled = false;
+                DexteritySecondPlayer.IsEnabled = false;
+                LuckSecondPlayer.IsEnabled = false;
+                ConstitutionSecondPlayer.IsEnabled = false;
+                IntteligenceSecondPlayer.IsEnabled = false;
+                StrengthMinusSecondPlayer.IsEnabled = false;
+                DexterityMinusSecondPlayer.IsEnabled = false;
+                LuckMinusSecondPlayer.IsEnabled = false;
+                ConstitutionMinusSecondPlayer.IsEnabled = false;
+                IntelligenceMinusSecondPlayer.IsEnabled = false;
+                StrengthPlusSecondPlayer.IsEnabled = false;
+                DexterityPlusSecondPlayer.IsEnabled = false;
+                LuckPlusSecondPlayer.IsEnabled = false;
+                ConstitutionPlusSecondPlayer.IsEnabled = false;
+                IntelligencePlusSecondPlayer.IsEnabled = false;
+                crtValueSecondPlayer.Text = $"{(int.Parse(LuckSecondPlayer.Text) - int.Parse(Luck.Text) * 0.5) * 0.1}";
+                evasionValueSecondPlayer.Text = $"{(int.Parse(DexteritySecondPlayer.Text) - int.Parse(Dexterity.Text)) * 0.1}";
+                breakoutsValueSecondPlayer.Text = $"{(int.Parse(StrengthSecondPlayer.Text) - int.Parse(Strength.Text)) * 0.05}";
+                Ready2.Content = "Unready";
+                ReadySecond = true;
+            }
+            else if (Ready2.Content.ToString() == "Unready")
+            {
+                NameSecondPlayer.IsEnabled = true;
+                StrengthSecondPlayer.IsEnabled = true;
+                DexteritySecondPlayer.IsEnabled = true;
+                LuckSecondPlayer.IsEnabled = true;
+                ConstitutionSecondPlayer.IsEnabled = true;
+                IntteligenceSecondPlayer.IsEnabled = true;
+                StrengthMinusSecondPlayer.IsEnabled = true;
+                DexterityMinusSecondPlayer.IsEnabled = true;
+                LuckMinusSecondPlayer.IsEnabled = true;
+                ConstitutionMinusSecondPlayer.IsEnabled = true;
+                IntelligenceMinusSecondPlayer.IsEnabled = true;
+                StrengthPlusSecondPlayer.IsEnabled = true;
+                DexterityPlusSecondPlayer.IsEnabled = true;
+                LuckPlusSecondPlayer.IsEnabled = true;
+                ConstitutionPlusSecondPlayer.IsEnabled = true;
+                IntelligencePlusSecondPlayer.IsEnabled = true;
+                Ready2.Content = "Ready";
+                ReadySecond = false;
+            }
+            
+            if(ReadyFirst == true && ReadySecond == true)
+            {
+                FightB.IsEnabled = true;
+            }
+            else
+            {
+                FightB.IsEnabled = false;
+            }
+        }       
+
+        private void Ready1_Click(object sender, RoutedEventArgs e)
+        {
+            List<FighterModelFirstPlayer> fighters = new List<FighterModelFirstPlayer>();
+            List<string> fightersName = new List<string>();
+            if (Ready1.Content.ToString() == "Ready")
+            {
+                Name.IsEnabled = false;
+                Strength.IsEnabled = false;
+                Dexterity.IsEnabled = false;
+                Luck.IsEnabled = false;
+                Constitution.IsEnabled = false;
+                Intteligence.IsEnabled = false;
+                StrengthMinus.IsEnabled = false;
+                DexterityMinus.IsEnabled = false;
+                LuckMinus.IsEnabled = false;
+                ConstitutionMinus.IsEnabled = false;
+                IntelligenceMinus.IsEnabled = false;
+                StrengthPlus.IsEnabled = false;
+                DexterityPlus.IsEnabled = false;
+                LuckPlus.IsEnabled = false;
+                ConstitutionPlus.IsEnabled = false;
+                IntelligencePlus.IsEnabled = false;
+                crtValue.Text = $"{(int.Parse(Luck.Text) - int.Parse(LuckSecondPlayer.Text) * 0.5) * 0.1}";
+                evasionValue.Text = $"{(int.Parse(Dexterity.Text) - int.Parse(DexteritySecondPlayer.Text)) * 0.1}";
+                breakoutsValue.Text = $"{(int.Parse(Strength.Text) - int.Parse(StrengthSecondPlayer.Text)) * 0.05}";
+                Ready1.Content = "Unready";
+                ReadyFirst = true;
+                FighterModelFirstPlayer fighter = new FighterModelFirstPlayer(Name.Text, int.Parse(lvlValue.Text), int.Parse(Strength.Text), int.Parse(Dexterity.Text), int.Parse(Luck.Text), int.Parse(Constitution.Text), int.Parse(Intteligence.Text));
+                fighters.Add(fighter);
+                fightersName.Add(Name.Text);
+                Units.ItemsSource = fightersName;
+            }
+            else if (Ready1.Content.ToString() == "Unready")
+            {
+                Name.IsEnabled = true;
+                Strength.IsEnabled = true;
+                Dexterity.IsEnabled = true;
+                Luck.IsEnabled = true;
+                Constitution.IsEnabled = true;
+                Intteligence.IsEnabled = true;
+                StrengthMinus.IsEnabled = true;
+                DexterityMinus.IsEnabled = true;
+                LuckMinus.IsEnabled = true;
+                ConstitutionMinus.IsEnabled = true;
+                IntelligenceMinus.IsEnabled = true;
+                StrengthPlus.IsEnabled = true;
+                DexterityPlus.IsEnabled = true;
+                LuckPlus.IsEnabled = true;
+                ConstitutionPlus.IsEnabled = true;
+                IntelligencePlus.IsEnabled = true;
+                Ready1.Content = "Ready";
+                ReadyFirst = false;
+            }
+            
+
+            if (ReadyFirst == true && ReadySecond == true)
+            {
+                FightB.IsEnabled = true;
+            }
+            else
+            {
+                FightB.IsEnabled = false;
+            }
+        }
+
+        private void Units_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach(var item in fighters)
+            {
+                if (Units.SelectedValue.ToString() == item.Name)
+                {
+                    Name.Text = item.Name;
+                    Strength.Text = item.Strength.ToString();
+                    Dexterity.Text = item.Dexterity.ToString();
+                    Luck.Text = item.Luck.ToString();
+                    Constitution.Text = item.Constitution.ToString();
+                    Intteligence.Text = item.Intelligence.ToString();
+                    lvlValue.Text = item.LVL.ToString();
+
+                }
             }
         }
     }
